4585cea7656119dd6e3ab08dd58099cdb6286626
meghanathul <92531334+meghanathul@users.noreply.github.com>
2022-03-23 19:55:03 +0530

    js-que

---
+++ script.js
@@ -0,0 +1,53 @@
+// var age=30;
+// if (age>30) {
+//    console.log('Adult') 
+// } else {
+//     console.log('old')
+// }
+// var age=5;
+// if (age>15) {
+//     console.log('Young');
+// } 
+// else if((age<15)&&(age>30)) {
+//   console.log('Adult')  
+// }
+// else{
+//     console.log('old')
+// }
+// var c=5;
+// switch (c) {
+//     case(0):{
+//         console.log('Monday')
+//     }
+//         break;   
+//     case(1):{
+//         console.log('Tuesday')
+//     }
+//         break;   
+//     case(2):{
+//         console.log('Wednesday')
+//     }
+//         break;   
+//     case(3):{
+//         console.log('Thusday')
+//     }
+//         break;   
+//     case(4):{
+//         console.log('Friday')
+//     }  
+//         break;
+        
+//     case(5):{
+//         console.log('Saterday')
+//     }
+//         break;
+//         default:
+//         console.log('Tuesday')
+        
+// }
+
+
+
+for(i=1;i<=10;i++){
+console.log(13+'*'+i+'='+13*i);
+}
